b56172455ac863be118e7bb9547999543eba4fab
yazukii <yvanix04@gmail.com>
2023-09-13 18:30:49 +0200

    added printlist

--- srcs/Main/main.c
+++ srcs/Main/main.c
@@ -17,28 +17,36 @@
 //	return (!ft_strncmp(str, "|", 1));
 //}
 
+void printlist(t_parsing bag)
+{
+    printf("%s\n", bag.p_head->pre_tokken);
+    while (bag.p_head->next)
+    {
+        bag.p_head = bag.p_head->next;
+        printf("%s\n", bag.p_head->pre_tokken);
+    }
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	t_parsing	*bag;
-	t_list_pre	*current;
+    t_parsing	*bag;
 
-	(void) argv;
-	(void) argc;
-	bag = NULL;
-	rl_initialize();
-	bag = init_parseur(bag, envp, TRUE);
-	while (1)
-	{
-		bag->input = readline(BLU"minishell$ "RESET);
-		if (*bag->input)
-		{
-			add_history(bag->input);
-			parseur(bag);
-			current = ft_pre_lstlast(bag->p_head);
-			printf("%s\n", current->pre_tokken);
-			free(bag->input);
-		}
-		bag = init_parseur(bag, envp, FALSE);
-	}
-	return (0);
+    (void) argv;
+    (void) argc;
+    bag = NULL;
+    rl_initialize();
+    bag = init_parseur(bag, envp, TRUE);
+    while (1)
+    {
+        bag->input = readline(BLU"minishell$ "RESET);
+        if (*bag->input)
+        {
+            add_history(bag->input);
+            parseur(bag);
+            printlist(*bag);
+            free(bag->input);
+        }
+        bag = init_parseur(bag, envp, FALSE);
+    }
+    return (0);
 }
